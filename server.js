b7e4feec4b4ed268443a20cd2ad657c01c473fe8
unknown <a.ekaterina323@gmail.com>
2021-10-12 21:41:44 +0300

    add example

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = new Koa();
 app.use(cors());
 app.use(koaBody({json: true}));
 
-const notes = [];
+const notes = [{id: 1, content: 'lalala'}];
 let nextId = 1;
 
 const router = new Router();
